a3e1a577127636ffb5f73997c3caaa94dc33919f
Evandro Calado <evandrocalado07@gmail.com>
2024-06-28 22:51:18 -0300

    chore: CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,73 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_api_todo.app import app
-
-
-def test_app_should_return_hello_world():
-    client = TestClient(app)
-
+def test_app_should_return_hello_world(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello World'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'test',
+            'email': 'test@example.com',
+            'password': 'test',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'test',
+        'email': 'test@example.com',
+    }
+
+
+def test_get_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'username': 'test',
+                'email': 'test@example.com',
+            },
+        ],
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'test2',
+            'email': 'test2@example.com',
+            'password': 'test2',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'test2',
+        'email': 'test2@example.com',
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
+
+
+def test_delete_user_not_found(client):
+    response = client.delete('/users/3')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
