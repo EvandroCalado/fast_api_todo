d7064810b9e883d2b5b33f800841b887ba2deb75
Evandro Calado <evandrocalado07@gmail.com>
2024-07-05 10:23:16 -0300

    chore: add authorization and authentication

--- fast_api_todo/app.py
+++ fast_api_todo/app.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
@@ -8,9 +9,16 @@ from fast_api_todo.database import get_session
 from fast_api_todo.models import User
 from fast_api_todo.schemas import (
     Message,
+    TokenSchema,
     UserListSchema,
     UserPublicSchema,
     UserSchema,
+)
+from fast_api_todo.security import (
+    create_access_token,
+    get_current_user,
+    get_password_hash,
+    verify_password,
 )
 
 app = FastAPI()
@@ -41,7 +49,11 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
                 detail="Email already existis",
             )
 
-    db_user = User(username=user.username, email=user.email, password=user.password)
+    db_user = User(
+        username=user.username,
+        email=user.email,
+        password=get_password_hash(user.password),
+    )
 
     session.add(db_user)
     session.commit()
@@ -52,7 +64,9 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
 
 @app.get("/users/", status_code=HTTPStatus.OK, response_model=UserListSchema)
 def get_users(
-    session: Session = Depends(get_session), limit: int = 10, offset: int = 0
+    session: Session = Depends(get_session),
+    limit: int = 10,
+    offset: int = 0,
 ):
     users = session.scalars(select(User).limit(limit).offset(offset))
 
@@ -75,32 +89,57 @@ def get_user(user_id: int, session: Session = Depends(get_session)):
     response_model=UserPublicSchema,
 )
 def update_user(
-    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+    user_id: int,
+    user: UserSchema,
+    session: Session = Depends(get_session),
+    current_user=Depends(get_current_user),
 ):
-    db_user = session.scalar(select(User).where(User.id == user_id))
+    if current_user.id != user_id:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST, detail="Not enough permission"
+        )
 
-    if not db_user:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="User not found")
+    current_user.username = user.username
+    current_user.email = user.email
+    current_user.password = get_password_hash(user.password)
 
-    db_user.username = user.username
-    db_user.email = user.email
-    db_user.password = user.password
-
-    session.add(db_user)
+    session.add(current_user)
     session.commit()
-    session.refresh(db_user)
+    session.refresh(current_user)
 
-    return db_user
+    return current_user
 
 
 @app.delete("/users/{user_id}", response_model=Message)
-def delete_user(user_id: int, session: Session = Depends(get_session)):
-    db_user = session.scalar(select(User).where(User.id == user_id))
+def delete_user(
+    user_id: int,
+    session: Session = Depends(get_session),
+    current_user=Depends(get_current_user),
+):
+    if current_user.id != user_id:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST, detail="Not enough permission"
+        )
 
-    if not db_user:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="User not found")
-
-    session.delete(db_user)
+    session.delete(current_user)
     session.commit()
 
     return {"message": "User deleted"}
+
+
+@app.post("/token", response_model=TokenSchema)
+def login_for_acess_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(get_session),
+):
+    user = session.scalar(select(User).where(User.email == form_data.username))
+
+    if not user or not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail="Incorrect email or password",
+        )
+
+    access_token = create_access_token(data={"sub": user.email})
+
+    return {"access_token": access_token, "token_type": "Bearer"}
--- tests/test_app.py
+++ tests/test_app.py
@@ -118,9 +118,10 @@ def test_get_user_if_not_existis(client, user):
     }
 
 
-def test_update_user(client, user):
+def test_update_user(client, user, token):
     response = client.put(
-        "/users/1",
+        f"/users/{user.id}",
+        headers={"Authorization": f"Bearer {token}"},
         json={
             "username": "test2",
             "email": "test2@example.com",
@@ -130,7 +131,7 @@ def test_update_user(client, user):
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        "id": 1,
+        "id": user.id,
         "username": "test2",
         "email": "test2@example.com",
     }
@@ -146,12 +147,30 @@ def test_update_user_if_not_existis(client, user):
         },
     )
 
-    assert response.status_code == HTTPStatus.NOT_FOUND
-    assert response.json() == {"detail": "User not found"}
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {"detail": "Not authenticated"}
 
 
-def test_delete_user(client, user):
-    response = client.delete("/users/1")
+def test_update_another(client, user, token):
+    response = client.put(
+        "/users/2",
+        headers={"Authorization": f"Bearer {token}"},
+        json={
+            "username": "test2",
+            "email": "test2@example.com",
+            "password": "test2",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {"detail": "Not enough permission"}
+
+
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f"/users/{user.id}",
+        headers={"Authorization": f"Bearer {token}"},
+    )
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "User deleted"}
@@ -160,5 +179,38 @@ def test_delete_user(client, user):
 def test_delete_user_not_found(client):
     response = client.delete("/users/3")
 
-    assert response.status_code == HTTPStatus.NOT_FOUND
-    assert response.json() == {"detail": "User not found"}
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {"detail": "Not authenticated"}
+
+
+def test_delete_another_user(client, token):
+    response = client.delete(
+        "/users/3",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {"detail": "Not enough permission"}
+
+
+def test_get_token(client, user):
+    response = client.post(
+        "/token",
+        data={"username": user.email, "password": user.clean_password},
+    )
+
+    token = response.json()
+
+    assert response.status_code == HTTPStatus.OK
+    assert token["token_type"] == "Bearer"
+    assert "access_token" in token
+
+
+def test_login_with_invalid_user(client):
+    response = client.post(
+        "/token",
+        data={"username": "invalid@test.com", "password": "invalid"},
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {"detail": "Incorrect email or password"}
